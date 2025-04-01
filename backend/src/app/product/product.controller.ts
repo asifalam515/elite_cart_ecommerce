99272f606e4deff24f1873639f13201b6bf72279
asifalam515 <asibulalam515@gmail.com>
2025-04-01 23:32:53 +0600

    json added

--- backend/src/app/product/product.controller.ts
+++ backend/src/app/product/product.controller.ts
@@ -3,8 +3,16 @@ import { ProductServices } from "./product.service";
 
 const createProduct = async (req: Request, res: Response) => {
   const { product: products } = req.body;
+  const sellerId = req.body.products.seller;
+
+  if (!sellerId) {
+    return res.status(400).json({ message: "Seller ID is required" });
+  }
   try {
-    const newProduct = await ProductServices.createProductToDB(products);
+    const newProduct = await ProductServices.createProductToDB(
+      products,
+      sellerId
+    );
     res.status(200).json({
       message: "New product Created",
       data: newProduct,
--- backend/src/app/product/product.service.ts
+++ backend/src/app/product/product.service.ts
@@ -1,8 +1,19 @@
+import { userModel } from "../user/user.model";
 import { IProduct } from "./product.interface";
 import { ProductModel } from "./product.model";
 
-const createProductToDB = async (product: IProduct) => {
-  return await ProductModel.create(product);
+const createProductToDB = async (productData: IProduct, sellerId: string) => {
+  //create the product
+  const product = await ProductModel.create({
+    ...productData,
+    seller: sellerId,
+  });
+  await userModel.User.findByIdAndUpdate(
+    sellerId,
+    { $push: { products: product.id } },
+    { new: true }
+  );
+  return product;
 };
 
 export const ProductServices = { createProductToDB };
